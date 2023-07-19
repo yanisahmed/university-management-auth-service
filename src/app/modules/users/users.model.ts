cd531b70846eec4da117bcc413913ca7aa0fa4b9
Yanis Ahmed <yanisahmed12@gmail.com>
2023-07-19 11:32:05 +0600

    [Dome] User service

---
+++ src/app/modules/users/users.model.ts
@@ -0,0 +1,19 @@
+import { Model, Schema, model } from 'mongoose'
+import { IUser } from './users.interface'
+
+// Create a new Model type that knows about IUserMethods...
+type UserModel = Model<IUser, object>
+
+// 2. Create a Schema corresponding to the document interface.
+const userSchema = new Schema<IUser>(
+  {
+    id: { type: String, required: true, unique: true },
+    role: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  },
+)
+
+export const User = model<IUser, UserModel>('User', userSchema)
