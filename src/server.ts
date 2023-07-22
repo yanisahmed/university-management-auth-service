5e75136621cf4d067e8cf427b038a0608d80807d
Yanis Ahmed <yanisahmed12@gmail.com>
2023-07-22 15:10:17 +0600

    [Done] Separate Log Files

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
-import logger from './shared/logger'
+import { logger, errorLogger } from './shared/logger'
 
 async function boostrap() {
   try {
@@ -15,7 +15,7 @@ async function boostrap() {
     })
   } catch (err) {
     // console.log(`Database Connection Failed`, err)
-    logger.info(`Database Connection Failed`, err)
+    errorLogger.info(`Database Connection was Failed`, err)
   }
 }
 
--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,13 +1,38 @@
-import winston from 'winston'
+import { createLogger, format, transports } from 'winston'
+import path from 'path'
+const { combine, timestamp, label, printf } = format
 
-const logger = winston.createLogger({
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  const date = new Date(timestamp)
+  const hour = date.getHours()
+  const minutes = date.getMinutes()
+  const seconds = date.getSeconds()
+
+  return `${date.toDateString()} : ${hour} : ${minutes}: ${seconds} [${label}] ${level} ${message}`
+})
+
+const logger = createLogger({
   level: 'info',
-  format: winston.format.json(),
+  format: combine(label({ label: 'PH' }), timestamp(), myFormat),
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
+    new transports.Console(),
+    new transports.File({
+      filename: path.join(process.cwd(), 'log', 'winston', 'success.log'),
+      level: 'info',
+    }),
   ],
 })
 
-export default logger
+const errorLogger = createLogger({
+  level: 'info',
+  format: combine(label({ label: 'PH' }), timestamp(), myFormat),
+  transports: [
+    new transports.Console(),
+    new transports.File({
+      filename: path.join(process.cwd(), 'log', 'winston', 'error.log'),
+      level: 'error',
+    }),
+  ],
+})
+
+export { logger, errorLogger }
